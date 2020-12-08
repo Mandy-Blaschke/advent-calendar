761cbf2d0839920c92960525b3f80a8c07cff7b3
Mandy Blaschke <post@mandy-blaschke.de>
2020-12-08 14:53:10 +0100

    first working state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'advent';
+export class AppComponent implements OnInit {
+
+  doors: Door[] = [];
+  start = 'Ich liebe Dich, weil Du';
+  end = 'bist!';
+  mid: string[] = [
+    '  clever', '  geduldig', '  liebevoll', '  zärtlich',
+    '  großartig', '  mutig', '  witzig', '  tolerant',
+    '  begehrenswert', '  gutmütig', '  mitreißend', '  ehrlich',
+    '  treu', '  herzlich', '  verständnisvoll', '  immer für mich da',
+    '  tollpatschig', '  so süß', '  perfekt für mich', '  fantasievoll',
+    '  wunderbar', '  Harmonie liebend', '  ein guter Mensch', '  humorvoll',
+  ];
+
+
+  ngOnInit(): void {
+    this.createCards();
+  }
+
+  createCards(): void {
+    for (let i = 1; i <= this.mid.length; i++) {
+      this.doors.push(
+        {
+          number: i,
+          isOpen: true,
+        }
+      );
+    }
+  }
+
+  checkDate(card: Door): boolean {
+    if (new Date() >= new Date(2020, 11, card.number)) {
+      return true;
+    }
+  }
+
+
+  toggle(card: Door): void {
+    if (this.checkDate(card)) {
+      card.isOpen = !card.isOpen;
+    }
+  }
+}
+
+interface Door {
+  number: number;
+  isOpen: boolean;
 }
